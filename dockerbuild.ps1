6704c942046cac3a0bea07e5951632438cd84748
Nathan Carlson <carl4403@umn.edu>
2024-08-06 09:48:31 -0500

    Cleanup unused file

--- dockerbuild.ps1
+++
@@ -1,6 +0,0 @@
-$version = get-date -Format yyyyMMddHHmmss
-$image = "redfish-exporter"
-
-docker login keppel.eu-de-1.cloud.sap
-docker build . -t keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
-docker push keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
